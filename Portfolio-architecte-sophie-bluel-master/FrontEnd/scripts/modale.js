37d50eafd681230c28532cf7805d6d477d9ae05d
Emma-F-git <emma.fortat@yahoo.fr>
2025-03-06 15:55:47 +0100

    Modification modale

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts/modale.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/scripts/modale.js
@@ -4,7 +4,7 @@ const token = sessionStorage.getItem("token");
 
 const login = document.getElementById("login");
 const logout = document.getElementById("logout");
-const filters = document.getElementById("filters");
+let filters = document.getElementById("filters");
 const showButton = document.getElementById("showDialog");
 const favDialog = document.getElementById("favDialog");
 const outputBox = document.querySelector("output");
@@ -12,9 +12,10 @@ const selectEl = favDialog.querySelector("select");
 const confirmBtn = favDialog.querySelector("#confirmBtn");
 
 if (token) {
-  /*Si connexion administrateur bouton login caché et bouton logout affiché*/
-  login.style.display = "none";
-  logout.style.display = "block";
+  login.style.display = "none"; /*bouton login non affiché*/
+  logout.style.display = "block"; /*bouton logout affiché*/
+  filters.style.display = "none"; /*Boutons filtres galerie non affichés*/
+
   document.getElementById("logout").addEventListener("click", (event) => {
     event.preventDefault();
     sessionStorage.removeItem("token");
@@ -22,26 +23,13 @@ if (token) {
   });
 }
 
-if (filters) {
-  filters.style.display = "none";
-}
-
-// Le bouton « Mettre à jour les détails » ouvre la modale <dialog>
+// Le bouton « Modifier » ouvre la modale <dialog>
 showButton.addEventListener("click", () => {
   favDialog.showModal();
 });
-
-// L'entrée « Animal préféré » définit la valeur du bouton d'envoi.
-selectEl.addEventListener("change", (e) => {
-  confirmBtn.value = selectEl.value;
-});
-
-// Le bouton « Annuler » ferme la boîte de dialogue sans la soumettre en raison de l'attribut [formmethod="dialog"], ce qui déclenche un événement de fermeture.
-favDialog.addEventListener("close", (e) => {
-  outputBox.value =
-    favDialog.returnValue === "default"
-      ? "Pas de valeur retournée."
-      : `Valeur retournée : ${favDialog.returnValue}.`; // Vérifie la présence de "default" au lieu d'une chaîne vide
+// Le bouton "Fermer" ferme le dialogue
+closeDialog.addEventListener("click", () => {
+  dialog.close();
 });
 
 // Empêchez le bouton « Confirmer » de soumettre le formulaire par défaut et fermez la boîte de dialogue avec la méthode `close()`, qui déclenche l'événement "close".
